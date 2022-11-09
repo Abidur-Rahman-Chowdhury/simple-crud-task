6c35ab6928d1980b758017a10bb673427ec6e225
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2022-11-09 22:54:46 +0600

    done deleting minion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +29,13 @@ async function run() {
       res.send(result);
     });
       
+    // deleting minion   
+    app.delete('/Delete/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await minionCollection.deleteOne(query)
+        res.send(result)
+    })
 
   } finally {
   }
